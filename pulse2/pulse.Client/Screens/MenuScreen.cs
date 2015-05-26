b233882266ff0101104aedaa6f36bfb5ed191da5
Chouhos <williamjbr@outlook.com>
2015-05-26 14:09:37 +0100

    Updated OpenTK version.

---
+++ pulse2/pulse.Client/Screens/MenuScreen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Input;
+using pulse.Client.Graphics;
+
+namespace pulse.Client.Screens
+{
+    class MenuScreen : BaseScreen
+    {
+        private Quad _bg;
+        private Quad _button;
+
+        private double _timeTotal = 0;
+
+        public MenuScreen()
+        {
+            _bg = new Quad(0, 0, 1024, 768);
+            _bg.Texture = TextureManager.LoadImage("Assets\\bg.jpg");
+            _button = new Quad(300, 300, 300, 100);
+            _button.Texture = TextureManager.LoadImage("Assets\\bg.jpg");
+            _renderables.Add(_bg);
+
+            _name = "Menu Screen";
+        }
+
+        private double _velocity = 500;
+
+        public override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            var mouse = Mouse.GetState(0);
+
+            if (mouse[MouseButton.Left])
+            {
+                
+            }
+
+        }
+    }
+}
