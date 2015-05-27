4455e25155efbf9bd81a8ce8966ca859f407955c
aevv <matt@aevv.net>
2015-05-27 21:06:43 +0100

    PNC Conversion project

---
+++ pulse.PNCConverter/pulse.PNCConverter.ConsoleHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pulse.PNCConverter.Logic;
+
+namespace pulse.PNCConverter.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var control = new ConverterControl();
+        }
+    }
+}
