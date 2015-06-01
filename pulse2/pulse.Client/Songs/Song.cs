83b8e20bd0b491572d28f3e52e9ded1c548e3887
aevv <matt@aevv.net>
2015-06-01 22:28:58 +0100

    Move about chart loading

--- pulse2/pulse.Client/Songs/Song.cs
+++ pulse2/pulse.Client/Songs/Song.cs
@@ -1,9 +1,13 @@
-﻿namespace pulse.Client.Songs
+﻿using pulse.Client.Audio;
+
+namespace pulse.Client.Songs
 {
     public class Song
     {
         public string FileName { get; set; }
         public string GroupName { get; set; }
         public string GroupCreator { get; set; }
+
+        public Sound Sound { get; set; }
     }
 }
